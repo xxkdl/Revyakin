c9026fa9ec7d71f26e86ba67b957733d2e988a64
xxkdl <9217286@stud.nau.edu.ua>
2024-09-29 00:12:41 +0300

    йоу

--- labs/1.7/Source1.cpp
+++ labs/1.7/Source1.cpp
@@ -1,109 +1,99 @@
 ﻿#include <iostream>
 using namespace std;
 
-//1. Блочна структура програм
+// 1. Блочна структура програм
+void n11() {
+    int a = 10;
+    cout << "Блок 1, a = " << a << endl;
 
-//void blockFunction1() {
-//    int a = 10;
-//    cout << "Block 1, a = " << a << endl;
-//
-//    // Вкладений блок
-//    {
-//        int b = 20;
-//        cout << "Nested Block in Block 1, b = " << b << endl;
-//    }
-//}
-//
-//void blockFunction2() {
-//    int c = 30;
-//    cout << "Block 2, c = " << c << endl;
-//}
-//
-//int main() {
-//    cout << "Main Block" << endl;
-//    blockFunction1();
-//    blockFunction2();
-//
-//    // Вкладений блок
-//    {
-//        int d = 40;
-//        cout << "Nested Block in Main, d = " << d << endl;
-//    }
-//
-//    return 0;
-//}
+    // Вкладений блок
+    {
+        int b = 20;
+        cout << "Вкладений блок в Блоці 1, b = " << b << endl;
+    }
+}
 
-//2. Статичний розподіл пам'яті
+void n12() {
+    int c = 30;
+    cout << "Блок 2, c = " << c << endl;
+}
 
-//static int globalStaticVar = 100;  // Статична змінна поза main
-//
-//void functionWithStaticVar() {
-//    static int staticVar = 0;  // Статична змінна всередині блоку
-//    staticVar++;
-//    cout << "Static Var in function: " << staticVar << endl;
-//}
-//
-//int main() {
-//    cout << "Global Static Variable: " << globalStaticVar << endl;
-//
-//    int localVar = 50;
-//    cout << "Local Variable: " << localVar << endl;
-//
-//    functionWithStaticVar();
-//    functionWithStaticVar();
-//
-//    return 0;
-//}
+// 2. Статичний розподіл пам'яті
+static int globalStaticVar = 100;  
 
-//3. Динамічний розподіл пам'яті
+void n2() {
+    static int staticVar = 0;  
+    staticVar++;
+    cout << "Статична змінна в функції: " << staticVar << endl;
+}
 
-//int main() {
-//    int* dynamicVar1 = new int(5);  // Динамічний розподіл в main
-//    cout << "Dynamic Variable 1: " << *dynamicVar1 << endl;
-//
-//    // Вкладений блок
-//    {
-//        int* dynamicVar2 = new int(10);
-//        cout << "Dynamic Variable 2: " << *dynamicVar2 << endl;
-//        delete dynamicVar2;  // Звільнення пам'яті
-//    }
-//
-//    delete dynamicVar1;  // Звільнення пам'яті
-//    return 0;
-//}
-//
-////4. Операція глобального дозволу видимості імен
-//
-//int var = 10;  // Глобальна змінна
-//
-//void testFunction() {
-//    int var = 20;  // Локальна змінна
-//    cout << "Local var: " << var << endl;
-//    cout << "Global var using scope resolution: " << ::var << endl;  // Використання глобального імені
-//}
-//
-//int main() {
-//    testFunction();
-//    return 0;
-//}
+// 3. Динамічний розподіл пам'яті
+void n3() {
+    int* dynamicVar1 = new int(5);  
+    cout << "Динамічна змінна 1: " << *dynamicVar1 << endl;
 
-//5. Динамічний розподіл пам'яті в "купі"
+    // Вкладений блок
+    {
+        int* dynamicVar2 = new int(10);
+        cout << "Динамічна змінна 2: " << *dynamicVar2 << endl;
+        delete dynamicVar2;  
+    }
+
+    delete dynamicVar1;  
+}
+
+// 4. Операція глобального дозволу видимості імен
+void n4() {
+    int var = 20;  // Локальна змінна
+    cout << "Локальна змінна: " << var << endl;
+    cout << "Глобальна змінна за допомогою резолюції області: " << ::globalStaticVar << endl;  // Використання глобального імені
+}
+
+// 5. Динамічний розподіл пам'яті в "купі"
+void n5() {
+
+    int* heapVar1 = new int(15);
+    cout << "Heap змінна 1: " << *heapVar1 << endl;
+
+    int* heapVar2 = new int(25);
+    cout << "Heap змінна 2: " << *heapVar2 << endl;
+
+    delete heapVar1;
+    heapVar1 = nullptr;  
+}
 
 int main() {
-    // Перший вказівник
-    int* heapVar1 = new int(15);
-    cout << "Heap Variable 1: " << *heapVar1 << endl;
+    int choice;
+    do {
+        cout << "\nВиберіть завдання (1-5, 0 для виходу): ";
+        cin >> choice;
 
-    // Другий вказівник
-    int* heapVar2 = new int(25);
-    cout << "Heap Variable 2: " << *heapVar2 << endl;
-
-    // Звільнення пам'яті
-    delete heapVar1;
-    heapVar1 = nullptr;  // Щоб уникнути утворення сміття
-
-    // Не звільняємо пам'ять для heapVar2 для демонстрації сміття
-    // delete heapVar2;
+        switch (choice) {
+        case 1:
+            n11();
+            n12();
+            break;
+        case 2:
+            cout << "Глобальна статична змінна: " << globalStaticVar << endl;
+            n2 ();
+            n2();
+            break;
+        case 3:
+            n3();
+            break;
+        case 4:
+            n4();
+            break;
+        case 5:
+            n5();
+            break;
+        case 0:
+            cout << "Вихід." << endl;
+            break;
+        default:
+            cout << "Невірний вибір. Спробуйте ще раз." << endl;
+        }
+    } while (choice != 0);
 
     return 0;
 }
--- practicals/1.4/main.cpp
+++ practicals/1.4/main.cpp
@@ -2,71 +2,103 @@
 #include <cmath>
 using namespace std;
 
+
 //1. Знайти значення суми чисел від 1 до 5:
-//int main() {
-//    int sum = 0;
-//    for (int i = 1; i <= 5; i++) {
-//        sum += i;
-//    }
-//    cout << "Сума чисел від 1 до 5: " << sum << endl;
-//    return 0;
-//}
+void n1() {
+    int sum = 0;
+    for (int i = 1; i <= 5; i++) {
+        sum += i;
+    }
+    cout << "Сума чисел від 1 до 5: " << sum << endl;
+}
+
 
 //2. Знайти факторіал числа N :
-//int main() {
-//    int N, factorial = 1;
-//    cout << "Введіть число N: ";
-//    cin >> N;
-//
-//    for (int i = 1; i <= N; i++) {
-//        factorial *= i;
-//    }
-//
-//    cout << "Факторіал числа " << N << ": " << factorial << endl;
-//    return 0;
-//}
+void n2() {
+    int N, factorial = 1;
+    cout << "Введіть число N: ";
+    cin >> N;
+
+    for (int i = 1; i <= N; i++) {
+        factorial *= i;
+    }
+
+    cout << "Факторіал числа " << N << ": " << factorial << endl;
+}
 
 
 //3. Знайти суму всіх парних чисел на проміжку від 0 до N :
-//int main() {
-//    int N, sum = 0;
-//    cout << "Введіть число N: ";
-//    cin >> N;
-//
-//    for (int i = 0; i <= N; i += 2) {
-//        sum += i;
-//    }
-//
-//    cout << "Сума всіх парних чисел від 0 до " << N << ": " << sum << endl;
-//    return 0;
-//}
+void n3() {
+    int N, sum = 0;
+    cout << "Введіть число N: ";
+    cin >> N;
+
+    for (int i = 0; i <= N; i += 2) {
+        sum += i;
+    }
+
+    cout << "Сума всіх парних чисел від 0 до " << N << ": " << sum << endl;
+}
+
 
 //4. Знайти добуток перших n елементів арифметичної прогресії :
-//int main() {
-//    int a1, d, n, product = 1;
-//    cout << "Введіть перший член прогресії (a1): ";
-//    cin >> a1;
-//    cout << "Введіть різницю прогресії (d): ";
-//    cin >> d;
-//    cout << "Введіть кількість елементів (n): ";
-//    cin >> n;
-//
-//    for (int i = 0; i < n; i++) {
-//        product *= (a1 + i * d);
-//    }
-//
-//    cout << "Добуток перших " << n << " елементів прогресії: " << product << endl;
-//    return 0;
-//}
+void n4() {
+    int a1, d, n, product = 1;
+    cout << "Введіть перший член прогресії (a1): ";
+    cin >> a1;
+    cout << "Введіть різницю прогресії (d): ";
+    cin >> d;
+    cout << "Введіть кількість елементів (n): ";
+    cin >> n;
 
-//5. Протабулювати функцію f(x) = cos(x) на проміжку від 0 до π з кроком h = 0.01:
-int main() {
+    for (int i = 0; i < n; i++) {
+        product *= (a1 + i * d);
+    }
+
+    cout << "Добуток перших " << n << " елементів прогресії: " << product << endl;
+}
+
+
+//5. Протабулювати функцію f(x) = cos(x) на проміжку від 0 до п з кроком h = 0.01:
+void n5() {
     const double pi = 3.141592653589793;
     double h = 0.01;
 
     for (double x = 0; x <= pi; x += h) {
         cout << "f(" << x << ") = " << cos(x) << endl;
     }
+}
+
+int main() {
+    int choice;
+
+    do {
+        cout << "\nВиберіть завдання (1-5, 0 для виходу): ";
+        cin >> choice;
+
+        switch (choice) {
+        case 1:
+            n1();
+            break;
+        case 2:
+            n2();
+            break;
+        case 3:
+            n3();
+            break;
+        case 4:
+            n4();
+            break;
+        case 5:
+            n5();
+            break;
+        case 0:
+            cout << "Вихід." << endl;
+            break;
+        default:
+            cout << "Невірний вибір. Спробуйте ще раз." << endl;
+        }
+    } while (choice != 0);
 
     return 0;
 }
